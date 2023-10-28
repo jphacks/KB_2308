10c6e1ee70894870bb0da447c1518ba9cef3a31c
Naoto Nakanishi <nanikasi1024@gmail.com>
2023-10-28 10:49:19 +0900

    init swift project

---
+++ ios/UnMap/UnMap/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UnMap
+//
+//  Created by 中西直人 on 2023/10/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
